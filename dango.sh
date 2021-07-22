3bf7f7e3fb21e03b9fab81888955e2ba2374be3f
gsobell <82414189+gsobell@users.noreply.github.com>
2021-07-22 18:26:45 -0400

    GPL 3.0 Initial script commit

---
+++ dango.sh
@@ -0,0 +1,7 @@
+#launchlogo
+
+#menu options
+
+#save location
+
+gnugo | sed -e 's/(/\x1b[32;43m(\x1b[0m/g;s/)/\x1b[32;43m)\x1b[0m/g;s/X /\x1b[30;43m● \x1b[0m/g;s/O/\x1b[97;43m●\x1b[0m/g;s/\./\x1b[30;43m·\x1b[0m/g;s/+/\x1b[30;43m+\x1b[0m/g;s/ /\x1b[30;43m \x1b[0m/g;s/[1-9]/\x1b[30;43m&\x1b[0m/g;s/[1-9][0-9]/\x1b[30;43m&\x1b[0m/g'
