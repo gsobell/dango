dfd6b28d8cde41a1e82a17df64a7d3457c046c8d
gabriel <gsobell@users.noreply.github.com>
2021-07-27 11:54:36 -0400

    Added persistent settings

--- dango.sh
+++ dango.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "
 
 
@@ -8,11 +10,22 @@ echo "
 ●●     ○●○●         ○●●  ●●○     ●○●  ●●     ○●●   ●●       ●●
 ●○       ●○   ●○●●○○○●●  ●●       ●●  ●○      ○●●  ●○       ○●
  ●●     ○●●  ●●      ●○  ●○       ●○  ●●     ○●●   ●●       ●●
-  ○●○●●○ ●●   ○●○○○○ ●○  ○●       ○●    ○●○●○○●○     ○○●●●●○
+  ○●○●●○ ●●   ○●○○○○ ●○  ○●       ○●    ○●○●○○●○     ○○●●●●○ 
                                        ●○      ●●
                                          ●●○○○○
-"
-sleep 1
+" 
+
+if ! command -v gnugo &> /dev/null; then
+echo "A one time install of GNU Go is neccesary to continue"   
+        sudo pacman -S gnugo  ||    
+        sudo apt install gnugo ||
+        sudo yum install gnugo ||
+        sudo pkg install gnugo ||
+        sudo zypper install gnugo ||  
+        sudo install gnugo
+else sleep 1
+fi
+
 
 # Default settings
 level=10
@@ -26,7 +39,12 @@ outfile=
 
 size=19
 komi="6.5"
+theme=modern
+config=default
 
+# Load user's saved presets
+config="~/.config/dango/config"
+[[ -f ~/.config/dango/config ]] && . ~/.config/dango && is_config=user
 
 clear
 
@@ -44,7 +62,8 @@ echo -e ├"\e[1m Komi\e[0;36m $komi        \e[0m    (3)"
 echo -e ├"\e[1m AI Strength \e[0;36m $level\e[0m     (4)" 
 echo -e ├"\e[1m Board size \e[0;36m $size x $size \e[0m(5)"
 echo -e ├"\e[1m Save game? \e[0;36m $out \e[0m     (6)"
-echo
+echo -e ├"\e[1m Theme \e[0;36m $theme \e[0m      (7)"
+echo -e ├"\e[1m Save presets? \e[0;36m $is_config \e[0m     (8)"
 
 
 echo -e "Select a number to edit,\e[0;31m enter\e[0m to start game"
@@ -70,8 +89,8 @@ read option
                 done;
                 clear;
                 break;;
-        3) read -r -p "Chose a number of komi " komi; clear; break;;
-        4) read -r -p " " komi; clear; break;;
+        3) read -r -p "Chose a number of komi "      komi; clear; break;;
+        4) read -r -p "Select an AI strength (1-10)"level; clear; break;;
         5) read -r -p " " komi; clear; break;;
         6) read -r -p "Chose path to save game: " outfile ;
                 echo $outfile; echo $out;
@@ -80,7 +99,44 @@ read option
                 sleep 2
                 clear;
                 break;;
-        "")        
+        7) echo "Choose a theme"
+                echo "Color is only fully supported on rxvt"
+                echo "
+5 . . . . .     
+4 . . . . .     
+3 . . . . .     
+2 . . . . .     
+1 . . . . .     
+  A B C D E     A B C D E
+
+  Classic       Modern          Color
+                
+"
+                select theme in Classic Modern Color; do
+                case $theme in
+                Classic) break;;
+                Modern) break;;
+                Color) break;;
+                esac
+                done;
+                clear;
+                break;;
+        8) mkdir -p ~/.config/dango
+        touch ~/.config/dango/config
+        echo "level=$level
+              color=$color
+              ruleset=$ruleset
+              out=$out
+              outfile=$outfile
+              komi=$komi
+              theme=$theme" > ~/.config/dango/config
+
+        echo "Current settings saved"
+        sleep 1
+        clear;
+                break;;
+        "") echo "Let the game begin"; break;;
+        :q) exit; break;;
         esac
 done
 done 
